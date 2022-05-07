ec0067b0938dacfa85a8b9dcf23a5172ed31be77
Asaf <asaf.karavani1@gmail.com>
2022-05-07 13:38:13 +0300

    feat: added devops scripts

--- scripts/generate-yamls.sh
+++ scripts/generate-yamls.sh
@@ -2,9 +2,22 @@
 Green='\033[1;32m' # Green
 NC='\033[0m' # No Color
 
+echo
+echo Generating \".yaml\" files from the \".yaml.template\" files.
+echo
+
 for yaml_template in $(find ../ -name "*.yaml.template" -type f); do
     echo "Processing \"$yaml_template\"..."
+    
+    for match in $(cat "$yaml_template" | grep -o '\{[^}]*\}' ); do
+        env_variable=${match//"{"/}
+        env_variable=${env_variable//"}"/}
+        echo var: \"$env_variable\" replaced.
+    done
+    
+    
     yaml_file=${yaml_template//.template/}
     envsubst < "$yaml_template" > "$yaml_file"
     echo -e "${Green}Generated \"$yaml_file\".${NC}"
+    echo
 done
