eec757f533d5fd9b89feeb9333c9fe6e8f050e7b
Asaf <asaf.karavani1@gmail.com>
2022-05-11 03:12:29 +0300

    feat(database): added product

---
+++ services/database/prisma/migrations/20220511001040_added_product/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "quota" INTEGER NOT NULL,
+    "mainImageUrl" TEXT NOT NULL,
+    "Images" TEXT[],
+    "businessId" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductCategory" (
+    "productId" INTEGER,
+    "categoryId" INTEGER,
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;
