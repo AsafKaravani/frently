ba23fe0512856adfe66267c8fe55d4fe8570131e
Asaf <asaf.karavani1@gmail.com>
2022-05-16 02:04:50 +0300

    feat(sup app): set pagename

--- apps/suppliers-web-app/src/features/edit-business/edit-business-page.tsx
+++ apps/suppliers-web-app/src/features/edit-business/edit-business-page.tsx
@@ -1,7 +1,16 @@
 import { styled } from '@mui/material/styles';
+import { useOnInit } from '../../utils/hooks';
 import { StyleSheetMap } from '../../utils/types/index';
+import { useSetRecoilState } from 'recoil';
+import { atom_pageName } from '../../core/navigation/page-name.state';
 
 export function EditBusinessPage() {
+    const s_setPageName = useSetRecoilState(atom_pageName);
+
+    useOnInit(() => {
+        s_setPageName('עמוד עסק');
+    });
+
     return <Root className={classes.root}>EditBusinessPage</Root>;
 }
 
