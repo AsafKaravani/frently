887cce86481a90f6afe95cf19bc47f8825c8612b
Asaf <asaf.karavani1@gmail.com>
2022-06-05 23:20:41 +0300

    .

---
+++ services/database/prisma/migrations/20220605182540_added_parent_id_to_category/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "categoryId" INTEGER,
+ADD COLUMN     "parentId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
